3147b62af766953a745b80d402c875b862fc7e70
josecaseiro <jvcaseiro@hotmail.com>
2021-07-29 22:43:05 +0100

    Fix TextTheme error
    On dart 2.4.0 or above its gives error because of Theme.of(context).textTheme.body1 doesn't exists anymore. Just changed to Theme.of(context).textTheme.bodyText1.

--- lib/widgets/search_input.dart
+++ lib/widgets/search_input.dart
@@ -57,11 +57,13 @@ class SearchInputState extends State<SearchInput> {
       padding: EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         children: <Widget>[
-          Icon(Icons.search, color: Theme.of(context).textTheme.body1!.color),
+          Icon(Icons.search,
+              color: Theme.of(context).textTheme.bodyText1!.color),
           SizedBox(width: 8),
           Expanded(
             child: TextField(
-              decoration: InputDecoration(hintText: "Search place", border: InputBorder.none),
+              decoration: InputDecoration(
+                  hintText: "Search place", border: InputBorder.none),
               controller: this.editController,
               onChanged: (value) {
                 setState(() {
