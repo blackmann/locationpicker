29613afd29d6b854451e7feba81e4d6dcc98762b
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-09-10 05:46:16 +0200

    ...

--- lib/place_picker.dart
+++ lib/place_picker.dart
@@ -1,3 +1,4 @@
 export 'widgets/widgets.dart';
 export 'entities/entities.dart';
+export 'l10n/gen_l10n/place_picker_localizations.dart';
 export 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
